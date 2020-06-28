4befda0b598c5597d066edd4b46e0d56d2edb799
paul Harrington <paulharrington@pauls-MacBook-Pro-4.local>
2020-06-28 14:12:38 +0100

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-$('body').scrollspy({ target: '#navbar-list' })
+window.addEventListener("scroll", function () {
+  if (document.body.scrollTop === 0) {
+    var homeLink = document.getElementById('home-link');
+    //user is at the top of the page;
+    homeLink.style.color('rgba(0,0,0,.9)');
+  }
+});
