0101e52cf9670ceee45fb13ba15fb5e164167f56
Paul Harrington <paulharrington05@gmail.com>
2021-01-25 22:33:00 +0000

    Disable code in script.js that was causing error response due to faulty syntax

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
-window.addEventListener("scroll", function () {
-  if (document.body.scrollTop === 0) {
-    var homeLink = document.getElementById('home-link');
-    //user is at the top of the page;
-    homeLink.style.color('rgba(0,0,0,.9)');
-  }
-});
+// window.addEventListener("scroll", function () {
+//     var homeLink = document.getElementById('home-link');
+//     if (document.body.scrollTop === 0) {
+//         //user is at the top of the page;
+//         homeLink.style.color = 'rgba(0,0,0,.9)';
+//     } else if (document.body.scrollTop !== 0) {
+//         homeLink.style.color = 'rgba(0,0,0,.5)';
+//     }
+// });
